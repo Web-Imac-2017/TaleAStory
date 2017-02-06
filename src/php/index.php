29b097c5e879ec27c5e96ec35fbf12dbb94ecf09
masterloutre <williejunpow@gmail.com>
2017-02-06 23:46:38 +0100

    class Database : instanciation + connexion

--- src/php/index.php
+++ src/php/index.php
@@ -1,12 +1,18 @@
 <?php
-  $options = array(
-      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-  );
 
+  /*
   $pdo = new PDO('mysql:dbname=test;host=localhost', 'root', '', $options);
-
   $i = $pdo->query('SELECT COUNT(*) FROM test');
-  //var_dump($i->fetchColumn(0));
+  var_dump($i->fetchColumn(0));
+  */
+  require 'module_database.php';
+  $db = new Database();
+  var_dump($db);
 
-  require '../index.html';
+  $data = $db->sendQuery('SELECT * FROM tableaux');
+  echo '<p>test bdd :'.$data['titre'].'</p>';
+
+  //require '../index.html';
+
+
 ?>
