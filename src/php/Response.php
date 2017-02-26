d0ca6046de45dd1a6867f30d46abb7fac1e16d3f
CecilePoucet <funymiette@gmail.com>
2017-02-26 16:49:50 +0100

    Module response fonctionnel manque le changement de webroot

--- src/php/Response.php
+++ src/php/Response.php
@@ -3,8 +3,8 @@
 class Response
 {
   public static function generateIndex($param){
-    require"../header.html";
-    echo '<script type=\'text/javascript\'>',$param,'</script></head>';
+
+    require"../php/header.php";
     require"../body.html";
   }
 }
---
+++ src/php/header.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<head>
+  <title>Tale A Story</title>
+  <meta name="description" content="Soit le maitre de ta propre aventure dans Tale A Story" />
+  <link rel="stylesheet" type="text/css" href="assets/css/main.min.css">
+  <script type="text/javascript" src="assets/js/main.min.js"></script>
+  <script type="text/javascript"><?= json_encode($param) ?></script>
+</head>
