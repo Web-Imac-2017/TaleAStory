f6a65a89fbeb63f0b79b743fb122b1acad0a1cde
firegreen <steeve.v91@gmail.com>
2017-03-06 16:02:00 +0100

    commit before move to dev

--- src/php/Router.php
+++ src/php/Router.php
@@ -35,7 +35,7 @@ class Router {
   }
 
   public static function index(){
-    Response::generateIndex((object)array('userID' => '1',
+    Response::generateIndex((object)array(/*'userID' => '1',*/
                                           'userPseudo' => 'Marcel Patulacci',
                                           'userImgPath' => 'patulacci_tiny.jpg',
                                           'time' => '16h45'));
