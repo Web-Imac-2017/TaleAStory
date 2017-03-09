c30f03d79aacdb8bd7cad87348ec3d75d84e6f2a
laugiera <laugier.anne.a@gmail.com>
2017-03-09 13:29:18 +0100

    fix Form get field

---
+++ src/php/ItemControler.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'Item.php';
+require_once 'Form.php';
+require_once 'module_database.php';
+
+class ItemControler {
+
+  public static function addItem() {
+    echo var_dump($_POST, $_SERVER, $_GET);
+    $name = Form::getField("name");
+    /*var_dump($name);
+    exit();*/
+    //$imgpath = Form::uploadFile($file_input);
+    $imgpath = "truc";
+    $brief = Form::getField("brief");
+    $item = new Item($name, $imgpath, $brief);
+    //$item->save();
+    Response::jsonResponse($name);
+    Response::jsonResponse($_POST);
+  }
+
+  public static function updateItem() {
+    $name = Form::getField("name");
+    $imgpath = Form::uploadFile("itemImg");
+    $brief = Form::getField("brief");
+    $id = Form::getField("id");
+    $item = new Item($name, $imgpath, $brief);
+    $item->id = $id;
+    $entries = array();
+    if ($name != NULL)
+      array_push($entries, $name);
+    if ($imgpath != NULL)
+      array_push($entries, $name);
+    if ($brief != NULL)
+      array_push($entries, $name);
+    $item->update($entries);
+  }
+
+  public static function deleteItem() {
+    $id = Form::getField("id");
+    $item = new Item("", "", "");
+    $item->id = $id;
+    $item->delete();
+  }
+
+  echo var_dump($_POST);
+
+}
+
+ ?>
---
+++ src/php/StepControler.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once 'Step.php';
+require_once 'Form.php';
+require_once 'module_database.php';
+
+class StepControler {
+  public static function addStep() {
+    echo var_dump($_POST, $_SERVER, $_GET);
+    $imgpath = "truc";
+    $body = Form::getField("body");
+    $question = Form::getField("question");
+    $accepted = Form::getField("accepted");
+    $idType = Form::getField("idType");
+    $item = new Step($imgpath, $body, $question, $accepted, $idType);
+    //$item->save();
+    Response::jsonResponse(???);
+    Response::jsonResponse($_POST);
+  }
+
+  public static function updateStep() {
+    $imgpath = Form::uploadFile("stepImg");
+    $body = Form::getField("body");
+    $question = Form::getField("question");
+    $accepted = Form::getField("accepted");
+    $idType = Form::getField("idType");
+    $item = new Step($imgpath, $body, $question, $accepted, $idType);
+    $item->id = $id;
+    $entries = array();
+    if ($imgpath != NULL)
+      array_push($entries, $name);
+    if ($body != NULL)
+      array_push($entries, $name);
+    if ($question != NULL)
+      array_push($entries, $name);
+    if ($accepted != NULL)
+      array_push($entries, $name);
+    if ($idType != NULL)
+      array_push($entries, $name);
+    $item->update($entries);
+  }
+
+  public static function deleteStep() {
+    $id = Form::getField("id");
+    $item = new Step("", "", "", "","");
+    $item->id = $id;
+    $item->delete();
+  }
+
+  echo var_dump($_POST);
+
+  }
+
+ ?>
