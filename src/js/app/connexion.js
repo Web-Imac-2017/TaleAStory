451bac6d45436b7a59921c15a32f723e76a00955
firegreen <steeve.v91@gmail.com>
2017-03-11 15:12:59 +0100

    commit before pull origin dev

--- src/js/app/connexion.js
+++ src/js/app/connexion.js
@@ -1,16 +1,18 @@
-import React from 'react';
 import RouteComponent from '../utils/routecomponent'
+import React from 'react';
 import config from '../config';
 import {Link} from 'react-router';
+import User from '../model/user'
+import {AppContextTypes} from './app';
 
 export default RouteComponent({
-
+	contextTypes : AppContextTypes,
 	getInitialState() {
 		this.state = [];
 	    return {
 			null
 	    };
- 
+
 	},
 
 	handleChange(event) {
@@ -22,27 +24,23 @@ export default RouteComponent({
 	handleSubmit(event) {
 		/* alert(this.state.login + '-' + this.state.password ); */
 		event.preventDefault();
-		/*this.context.router.push(config.path('home'));*/
-		if (this.context.requestedPage == null || this.context.requestedPage == undefined ) {
-			this.context.requestedPage = config.path('home');
-		}
-		this.context.router.push(this.context.requestedPage)
+		this.context.setUser(new User(1,this.state.login, 'default_tiny.png'));
+		this.context.goRequestedPage();
 	},
-
-    render(){
-		return  <div className="homePageConnectionScreen">
-					<div className="content">
-						<div className="block">
-							<h1 className="element pageTitle">Connexion</h1>
-							<form className="element" onSubmit={this.handleSubmit}>
-								<span><input name="login" type="text" placeholder="Login" value={this.state.login} onChange={this.handleChange} ref="login" /></span>  
-								<span><input name="password" type="text" placeholder="Password" value={this.state.password} onChange={this.handleChange} ref="password" /></span>
-								<span className="button" ><input className="submit" type="submit" value="Connexion"/></span>
-							</form>
-							<p>Pas encore de compte ?<Link className="link linkAnim" to={config.path('sign/up')}>Inscrivez-vous !</Link></p>
-						</div>
+  render(){
+	return  <div className="homePageConnectionScreen">
+				<div className="content">
+					<div className="block">
+						<h1 className="element pageTitle">Connexion</h1>
+						<form className="element" onSubmit={this.handleSubmit}>
+							<span><input name="login" type="text" placeholder="Login" value={this.state.login} onChange={this.handleChange} ref="login" /></span>
+							<span><input name="password" type="text" placeholder="Password" value={this.state.password} onChange={this.handleChange} ref="password" /></span>
+							<span className="button" ><input className="submit" type="submit" value="Connexion"/></span>
+						</form>
+						<p>Pas encore de compte ?<Link className="link linkAnim" to={config.path('sign/up')}>Inscrivez-vous !</Link></p>
 					</div>
 				</div>
+			</div>
 
-    }
+  }
 });
